326601b5d6235b3cceb6010c136d0f9bddf87f6e
shubham <kumarsinghshubham32111@gmail.com>
2019-10-02 18:32:53 +0530

    changes made

--- a.py
+++ a.py
@@ -24,3 +24,40 @@ res = list(set(map(lambda i: tuple(sorted(i)), l2)))
 l3 = [[-11, 0, 11], [-11, 11, 0], [-11, 0, 11], [-11, 2, -11], [-11, 2, -11], [-11, -11, 2]]
 r = set(map(lambda a : tuple(sorted(a)), l3))
 print(r)
+
+l4 = [1, 5, 3, 6, 3, 5, 6, 1]
+l4 = list(set(l4))
+print(l4)
+
+#swap the first and last element of the list
+l5 = [12, 35, 9, 56, 24] 
+g = l5[0], l5[-1]
+l5[-1], l5[0] = g
+print(l5)
+
+start, *middle, last = l5
+l5 = [last, *middle, start]
+print(l5)
+
+e = l5.pop()
+b = l5[0]
+l5.append(b)
+l5[0] = e
+print(l5)
+
+def swapPosition(l, p1, p2):
+  l[p1], l[p2] = l[p2], l[p1]
+  print(l)
+
+swapPosition(l5, 1, 2)
+
+def removeIthWord(l, word, n):
+  c = 0
+  for i in range(len(l)):
+    if l[i] == word:
+      c+=1
+      if c == n:
+        del l[i]
+  print(l)
+
+removeIthWord(['geeks', 'for', 'geeks'], 'geeks', 2)
